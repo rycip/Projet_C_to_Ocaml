533f38932ccd50297bc93a1e2db80ce218b1424e
rycip <rycip>
2024-06-08 17:29:15 +0200

    Ajout fichier boucles

---
+++ src/boucles.c
@@ -0,0 +1,1 @@
+#include "boucles.h"
---
+++ src/boucles.h
@@ -0,0 +1,13 @@
+#ifndef BOUCLES_H
+#define BOUCLES_H
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// TODO
+
+#endif // !BOUCLES_H
